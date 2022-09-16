9df2e59dcb6eb8de9a65bbf387afd8e42a79facc
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-09-15 21:51:23 -0400

    lab2

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Switch, Text, TextInput, View, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,isDarkMode ? styles.darkMode : styles.lightMode]}>
+      <TextInput 
+        style={styles.input}
+        value={text} 
+        onChangeText={val => setText(val)} 
+        placeholder="Enter Your text"
+      />
+      <Text>Your Input : {text}</Text>
+      <TouchableHighlight 
+        style={styles.button} 
+      >
+      <Button 
+        color='black'
+        title="Clear" onPress={() => setText('')} 
+      />
+      </TouchableHighlight>
+      <Switch  
+        trackColor={{ false: "#767577", true: "FA7070" }}
+        thumbColor={isDarkMode ? "#000" : "#fff"}
+        onValueChange={() => setIsDarkMode(!isDarkMode)}
+        value={isDarkMode}
+        />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles =  StyleSheet.create({
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center' 
   },
+  darkMode:{
+    backgroundColor: '#181818',
+  },
+  lightMode:{
+    backgroundColor: '#fff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    borderRadius: 10,
+  },
+  button: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#EFEFEF',
+    color: '#fff',
+    borderRadius: 10,
+  }
 });
